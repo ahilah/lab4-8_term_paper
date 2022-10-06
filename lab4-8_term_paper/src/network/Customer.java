f774be115707c226af426bea01c8cd6cc6f67e67
ahilah <112408088+ahilah@users.noreply.github.com>
2022-10-06 22:10:53 +0300

    Add files via upload
    edited class Customer

--- lab4-8_term_paper/src/network/Customer.java
+++ lab4-8_term_paper/src/network/Customer.java
@@ -4,4 +4,24 @@ public class Customer {
     String name;
     String CustomerID;
 
+    public Customer(String name, String CustomerID) {
+        this.name = name;
+        this.CustomerID = CustomerID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCustomerID(String customerID) {
+        CustomerID = customerID;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", CustomerID='" + CustomerID + '\'' +
+                '}';
+    }
 }
